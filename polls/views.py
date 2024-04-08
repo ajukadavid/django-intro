55884d80921a4ea8cfb186bf5fc45add472111b7
ithiel883 <ajuka.zephiniah@gmail.com>
2024-04-08 23:13:12 +0100

    added another route

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def index(request):
+    return HttpResponse("Hello world you're at the polls index")
+def testing(request):
+    return HttpResponse("Hello welcome to testing")
